a4f205dd1739ffb51eb4776a417a92f6dec9ce1a
Kelci Mensah <kelcimensah@gmail.com>
2023-07-24 00:59:37 -0400

    adding more HTML elements

--- src/components/Element.tsx
+++ src/components/Element.tsx
@@ -1,13 +1,5 @@
 import { createElement } from "react";
 
-// function Greeting({ name }: { name: string }) {
-//   return (
-//     <h1 className="greeting">
-//       Hello <i>{name}</i>. Welcome!
-//     </h1>
-//   );
-// }
-
 export default function Element({
   element,
   exmpl,
@@ -16,18 +8,12 @@ export default function Element({
   exmpl: string;
 }) {
   function DynamicTag() {
-    return createElement(
-      `${ element }`,
-      { className: "example" },
-      `${ exmpl }`
-    );
+    return createElement(`${element}`, { className: "example" }, `${exmpl}`);
   }
 
-  //   var DynamicTag: string = `${element}` as keyof JSX.IntrinsicElements;
   return (
     <div id={element} className={element}>
       <h3 style={{ color: "#fffffe" }}>&lt;{element}&gt;</h3>
-      {/* <DynamicTag>{exmpl}</DynamicTag> */}
       <DynamicTag />
     </div>
   );
--- src/sections/Typography.tsx
+++ src/sections/Typography.tsx
@@ -3,16 +3,25 @@ import Element from "../components/Element";
 export default function Typography() {
   return (
     <div>
-      <a href="https://www.englishclub.com/reading/cr-hare-tortoise.php"><Element element="a" exmpl="https://www.englishclub.com/reading/cr-hare-tortoise.php"/></a>
-      <Element element="h1" exmpl="The Hare and the Tourtoise"/>
-      <Element element="h2" exmpl="The Hare and the Tourtoise"/>
-      <Element element="h3" exmpl="The Hare and the Tourtoise"/>
-      <Element element="h4" exmpl="The Hare and the Tourtoise"/>
-      <Element element="h5" exmpl="The Hare and the Tourtoise"/>
+      <a href="https://www.englishclub.com/reading/cr-hare-tortoise.php"><Element element="a" exmpl="https://www.englishclub.com/reading/cr-hare-tortoise.php" /></a>
+      <Element element="del" exmpl="The Hare thought this idea was impossible and he agreed to the proposal." />
+      <Element element="h1" exmpl="The Hare and the Tourtoise" />
+      <Element element="h2" exmpl="The Hare and the Tourtoise" />
+      <Element element="h3" exmpl="The Hare and the Tourtoise" />
+      <Element element="h4" exmpl="The Hare and the Tourtoise" />
+      <Element element="h5" exmpl="The Hare and the Tourtoise" />
       <Element element="h6" exmpl="One day the Hare laughed at the short feet and slow speed of the
           Tortoise."/>
+      <Element element="mark" exmpl="The Hare thought this idea was impossible and he agreed to the proposal." />
+        
       <Element element="p" exmpl="One day the Hare laughed at the short feet and slow speed of the
           Tortoise."/>
+      <Element element="pre" exmpl="But when he reached the end, he saw the Tortoise there already, sleeping comfortably after her effort.
+      - Aesop"/>
+      <Element element="q" exmpl="The Tortoise never stopped for a moment, walking slowly but steadily, right to the end of the course."/>
+      <Element element="samp" exmpl="The Hare ran fast and stopped to lie down for a rest."/>
+      <Element element="small" exmpl="This work is considered to be in the public domain in the United States."/>
+
     </div>
   );
 }
