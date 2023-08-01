c855ab1dc6edcb18f15ac5c4e541170209c49c91
Kelci Mensah <kelcimensah@gmail.com>
2023-08-01 15:15:09 -0400

    added a "none" text indicator for tags that don't have any attributes

--- src/components/Attribute.tsx
+++ src/components/Attribute.tsx
@@ -9,66 +9,89 @@ interface Props {
 
 export default function Attr({ required, optional, deprecated }: Props) {
     const ref = useRef<HTMLDivElement>(null);
-    console.log("test i" + ref);
+    // console.log("test i" + ref);
 
     function addAttributes() {
+        let noAttributes: boolean = false;  // defaulted to false meaning there might be some attributes
+
         const r_string = required!.toString();
         let r_arr = r_string.split(",");
-        if (r_string == "") { r_arr = [] }
+
+        // if no attributes inputted, you should actually empty the array
+        if (r_string == "") {
+            r_arr = []
+        }
 
         let o_string = optional!.toString();
         let o_arr = o_string.split(",");
-        if (o_string == "") { o_arr = []} 
+        if (o_string == "") {
+            o_arr = [];
+        }
 
         let d_string = deprecated!.toString();
         let d_arr = d_string.split(", ");
         if (d_string == "") { d_arr = [] }
-        
-        for (let i = 0; i < r_arr.length; i++) {r_arr[i] = r_arr[i].trim()}
-        for (let i = 0; i < o_arr.length; i++) {o_arr[i] = o_arr[i].trim()}
-        for (let i = 0; i < d_arr.length; i++) {d_arr[i] = d_arr[i].trim()}
 
-        r_arr = r_arr.sort();
-        o_arr = o_arr.sort();
-        d_arr = d_arr.sort();
+        if (d_arr.length + o_arr.length + r_arr.length == 0) {
+            noAttributes = true
+        }
 
-        useEffect(() => {
-            const element = ref.current;
-            console.log("test k" + element);
-            let node, attribute;
+        if (noAttributes == false) {    // add attribute badges if there is some badge
+            for (let i = 0; i < r_arr.length; i++) { r_arr[i] = r_arr[i].trim() }
+            for (let i = 0; i < o_arr.length; i++) { o_arr[i] = o_arr[i].trim() }
+            for (let i = 0; i < d_arr.length; i++) { d_arr[i] = d_arr[i].trim() }
 
-            if (r_arr.length >= 1) {
-                for (let i = 0; i < r_arr.length; i++) {
-                    node = document.createElement("p");
-                    node.className = "tag-attribute tag-required";
-                    attribute = document.createTextNode(`${r_arr[i]}`.trim());
-                    node.appendChild(attribute);
-                    element!.appendChild(node);
+            r_arr = r_arr.sort();
+            o_arr = o_arr.sort();
+            d_arr = d_arr.sort();
+
+            useEffect(() => {
+                const element = ref.current;
+                // console.log("test k" + element);
+                let node, attribute;
+
+                if (r_arr.length >= 1) {
+                    for (let i = 0; i < r_arr.length; i++) {
+                        node = document.createElement("p");
+                        node.className = "tag-attribute tag-required";
+                        attribute = document.createTextNode(`${r_arr[i]}`.trim());
+                        node.appendChild(attribute);
+                        element!.appendChild(node);
+                    }
                 }
-            }
 
-            if (o_arr.length > 0) {
-                for (let i = 0; i < o_arr.length; i++) {
-                    node = document.createElement("p");
-                    node.className = "tag-attribute tag-optional";
-                    attribute = document.createTextNode(`${o_arr[i]}`.trim());
-                    node.appendChild(attribute);
-                    element!.appendChild(node);
+                if (o_arr.length > 0) {
+                    for (let i = 0; i < o_arr.length; i++) {
+                        node = document.createElement("p");
+                        node.className = "tag-attribute tag-optional";
+                        attribute = document.createTextNode(`${o_arr[i]}`.trim());
+                        node.appendChild(attribute);
+                        element!.appendChild(node);
+                    }
                 }
-            }
 
-            if (d_arr.length > 0) {
-                for (let i = 0; i < d_arr.length; i++) {
-                    node = document.createElement("p");
-                    node.className = "tag-attribute tag-deprecated";
-                    attribute = document.createTextNode(`${d_arr[i]}`.trim());
-                    node.appendChild(attribute);
-                    element!.appendChild(node);
+                if (d_arr.length > 0) {
+                    for (let i = 0; i < d_arr.length; i++) {
+                        node = document.createElement("p");
+                        node.className = "tag-attribute tag-deprecated";
+                        attribute = document.createTextNode(`${d_arr[i]}`.trim());
+                        node.appendChild(attribute);
+                        element!.appendChild(node);
+                    }
                 }
-            }
-        }, []);
+            }, []);
+        } else {
+            useEffect(() => {
+                const element = ref.current;
+                let node, attribute;
 
-
+                node = document.createElement("p");
+                node.className = "tag-attribute tag-none";
+                attribute = document.createTextNode("none");
+                node.appendChild(attribute);
+                element!.appendChild(node);
+            }, []);
+        }
     }
 
     addAttributes();
